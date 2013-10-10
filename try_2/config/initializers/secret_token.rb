8b2011eb33c45e82e54bb0b349577aac8af020f3
PavelMix <pavel_mix@inbox.ru>
2013-10-10 11:01:25 +0300

    Initial commit

---
+++ try_2/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Try2::Application.config.secret_key_base = 'ef2a4337d85287c64d354de21681146f1a8c065da3fc8fc4c0df490dea44e473274dcab4dbf865f5a00cb2c89c6f5fd80265b2a2982561a439dd3a628a9e1946'
